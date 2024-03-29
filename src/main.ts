2dded6f4a00abd43b0a3b4336996a8319082ee94
I Putu Bagus Widia Predana <78652589+SeedONova@users.noreply.github.com>
2024-03-29 08:40:23 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+
+const app = express()
+
+app.use(urlencoded({
+    extended: false
+}))
+app.use(json())
+
+app.listen(8080, () => console.log('server is up and running on port 8080'))
