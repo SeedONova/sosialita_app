b8f04aa9eb75b3663fd49e585cc1247e61541ec7
I Putu Bagus Widia Predana <78652589+SeedONova@users.noreply.github.com>
2024-03-30 11:49:35 +0700

    creating user schema

---
+++ src/routers/auth/signup.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response, NextFunction } from 'express'
+
+const router = Router()
+
+router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
+    const {email, password} = req.body;
+})
+
+export { router as signupRouter }
