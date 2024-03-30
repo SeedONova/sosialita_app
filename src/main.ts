75a9da44db89be80e840801a8c723eedd4404025
I Putu Bagus Widia Predana <78652589+SeedONova@users.noreply.github.com>
2024-03-30 10:00:25 +0700

    complete models and routers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import * as dotenv from 'dotenv'
 dotenv.config();
 
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import { json, urlencoded } from 'body-parser'
 import mongoose from 'mongoose'
+import { newPostRouter,
+         deletePostRouter, 
+         updatePostRouter, 
+         showPostRouter, 
+         newCommentRouter, 
+         deleteCommentRouter 
+} from './routers'
 
 const app = express()
 
@@ -12,7 +19,33 @@ app.use(urlencoded({
 }))
 
 app.use(json())
-console.log('MONGO_URI:', process.env.MONGO_URI);
+
+app.use(newPostRouter)
+app.use(deletePostRouter)
+app.use(updatePostRouter)
+app.use(showPostRouter)
+app.use(newCommentRouter)
+app.use(deleteCommentRouter)
+
+app.all('*', (req, res, next) => {
+    const error = new Error('not found!') as CustomError;
+    error.status = 404
+    next()
+})
+
+declare global  {
+    interface CustomError extends Error{
+        status?: number
+    }
+}
+
+app.use((error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    if(error.status){
+        return res.status(error.status).json({ message: error.message });
+    }
+
+    res.status(500).json({message: 'something went wrong'})
+})
 
 const start = async () => {
     if(!process.env.MONGO_URI) throw new Error('MONGO_URI is required!')
